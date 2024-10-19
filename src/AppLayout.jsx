d0ac25424196f03432446e22d8ae2e368fcf1186
Vardaan Bhatia <vardaanbhatia55@gmail.com>
2024-10-19 20:38:04 +0530

    app layout

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "@/components/header";
+import Header from "@/components/Header";
 import Footer from "./components/Footer";
 
 // Layout component for the app, includes Header, Footer, and main content area
